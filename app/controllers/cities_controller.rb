54120ebe451cd82bf9ee80831bfdb397551e05fd
toxox <toxa1993@gmail.com>
2014-08-24 18:50:53 +0300

    Add basic search field

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,10 @@
 class CitiesController < ApplicationController
   def index
     @cities = City.all
+    if params[:search]
+      @city = City.search(params[:search])
+      redirect_to @city
+    end
   end
 
   def show
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,5 @@
 class City < ActiveRecord::Base
+  def self.search(query)
+    where("first_letter like ?", "%#{query}%").sample
+  end
 end
