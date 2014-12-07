d6e0f371e55aa56f59906ddb60928949ea49e8e3
toxox <toxa1993@gmail.com>
2014-12-07 21:11:34 +0200

    Add search params to api show method

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -2,10 +2,16 @@ class Api::V1::CitiesController < ApplicationController
   respond_to :json
 
   def index
-    respond_with City.all
+    if params[:search]
+      @city = City.search(params[:search])
+      respond_with @city
+    else
+      respond_with City.all
+    end
   end
 
   def show
+    @city = City.search(params[:search])
     respond_with City.find(params[:id])
   end
 end
